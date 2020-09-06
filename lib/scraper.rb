a7397c77398ddd76d000418bc055ba888c0a8bc2
Zackary Cammack <zack.cammack@gmail.com>
2020-09-06 18:00:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,8 +15,9 @@ class Scraper
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
+    binding.pry
   end
-  binding.pry
+  
 
 
   def get_courses

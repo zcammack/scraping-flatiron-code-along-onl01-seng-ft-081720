366f39ece54eaee22ced1b82bfa6daeb54209072
Zackary Cammack <zack.cammack@gmail.com>
2020-09-06 17:36:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,8 @@ require_relative './course.rb'
 class Scraper
 
   def get_page
-
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end
 
   def get_courses

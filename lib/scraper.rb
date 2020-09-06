5d3f114a38a49cb946dec2de851fc832295c088e
Zackary Cammack <zack.cammack@gmail.com>
2020-09-06 17:36:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,22 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+
+  def get_page
+
+  end
+
+  def get_courses
+
+  end
+
+  def make_courses
+
+  end
   
   def print_courses
     self.make_courses
